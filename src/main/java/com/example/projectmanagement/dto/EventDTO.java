6b2de3ed71579ca57b92b007ece8249420298d24
Anh Hoang The <hoangtheanhc2lp@gmail.com>
2024-06-08 17:47:08 +0700

    event dto

--- src/main/java/com/example/projectmanagement/dto/EventDTO.java
+++ src/main/java/com/example/projectmanagement/dto/EventDTO.java
@@ -1,4 +1,23 @@
 package com.example.projectmanagement.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Long taskId;
+    private String status;
+    private String priority;
+    private Timestamp startDate;
+    private Timestamp endDate;
 }
--- src/main/java/com/example/projectmanagement/entity/Event.java
+++ src/main/java/com/example/projectmanagement/entity/Event.java
@@ -1,4 +1,26 @@
 package com.example.projectmanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Table(name ="scheduler")
+@Entity
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Long taskId;
+    private String status;
+    private String priority;
+    private Timestamp startDate;
+    private Timestamp endDate;
 }
